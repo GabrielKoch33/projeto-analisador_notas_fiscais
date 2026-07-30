f533400f0e4dfad18ed5c30f2b8a3a3e059ebbd7
Gabriel Koch da Silva <gabrielkochsilva@gmail.com>
2026-07-30 12:03:21 -0300

    criado arquivos iniciais

---
+++ config.py
@@ -0,0 +1,6 @@
+# OCR (ler texto de imagem/PDF escaneado) = pytesseract	= Instalar o Tesseract-OCR (binário .exe, não é só pip)
+# PDF para imagem para o OCR ler = pdf2image = 	Instalar o Poppler for Windows e apontar o caminho do bin
+# Abrir/Gerar PDF de JPG = Pillow
+# Manipular PDFs = pypdf ou PyMuPDF
+# Interface = Tkinter ; .exe = PyInstaller 
+
